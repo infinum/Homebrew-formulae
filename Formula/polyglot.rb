20296e3d4dd4f71470dffe738194c1acf34576eb
donikv <donikv@gmail.com>
2018-07-20 11:31:46 +0200

    Changes tar url

--- Formula/polyglot.rb
+++ Formula/polyglot.rb
@@ -2,7 +2,7 @@ class Polyglot < Formula
     desc "Infinum's polyglot tool for iOS"
     homepage "https://github.com/infinum/iOS-Polyglot"
     #head "git@github.com:infinum/iOS-Polyglot.git", :using => :git
-    url "https://github.com/infinum/Homebrew-formulae/blob/master/Sources/ios-polyglot.tar.gz"
+    url "https://github.com/infinum/Homebrew-formulae/raw/master/Sources/ios-polyglot.tar.gz"
     sha256 "8601e75c70fb1eac522449ff59ef74fd34d376744bfa5b34fc44bd2ece34a4d7"
   
     def install
