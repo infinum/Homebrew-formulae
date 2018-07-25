c85ad90a3159d2534f1fdc0af40d7918c571b8bf
donikv <donikv@gmail.com>
2018-07-25 13:09:29 +0200

    Version up

--- Formula/polyglot.rb
+++ Formula/polyglot.rb
@@ -3,7 +3,7 @@ class Polyglot < Formula
     homepage "https://github.com/infinum/iOS-Polyglot"
     #head "git@github.com:infinum/iOS-Polyglot.git", :using => :git
     url "https://github.com/infinum/Homebrew-formulae/raw/master/Sources/ios-polyglot.tar.gz"
-    sha256 "248ba2c4113b5820f952a4bf4fa11783127dab2e30d79221c5b469c0179bdd92"
+    sha256 "0fc3c0c1a08eb73e1a0f59dd1b8a3ca6977d5c450e5d8542503e7124f6e1dea0"
   
     def install
       system "swift", "build", "-c", "release", "-v", "--build-path=#{bin}", "--disable-sandbox"
